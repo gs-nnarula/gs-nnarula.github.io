73368dcb4a9f42ba1613d886362216908b2e5143
Nischayy <nnarula@gainsight.com>
2023-05-04 17:26:10 +0530

    drag event

--- px-training/src/app/modules/widgets/widgets.component.ts
+++ px-training/src/app/modules/widgets/widgets.component.ts
@@ -1,4 +1,6 @@
+import { CdkDragDrop } from "@angular/cdk/drag-drop";
 import { Component } from "@angular/core";
+import { PxService } from "../services/px.service";
 
 @Component({
     selector: 'px-widgets',
@@ -6,5 +8,11 @@ import { Component } from "@angular/core";
     styleUrls: ['./widgets.component.scss']
 })
 export class WidgetsComponent {
+
+    constructor(private px: PxService) { }
+
+    drop(event: CdkDragDrop<any>, widgetNumber: number) {
+        this.px.track('Drag_Widget_Event', { widgetNumber });
+    }
 
 }
